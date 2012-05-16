3d9a7c2a8d197246074a83446774fafdfa247303
vaishakh.bargurreddy@gmail.com <vaishakh.bargurreddy@gmail.com@c63906e7-9778-56f8-ffda-514f3c1461fa>
2012-05-16 12:31:50 +0000

    Final Commitment Machas!

--- src/Square.h
+++ src/Square.h
@@ -1,3 +1,18 @@
+/*
+<Running Scared. A Terminal implementation of the Classic Pac man game using UNIX Signal, File and Process APIs>
+    Copyright (C) <2012>  <Samir Sheriff, Satvik N & Vaishakh B N >
+		<Code Kshetra>	
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+*/
 #ifndef _SQUARE_H
 #define _SQUARE_H
 
